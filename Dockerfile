c16a7b9f5ccc3cdd86cedaccb901f66c3c28652d
Arturo0911 <anegreiross@outlook.com>
2022-06-24 00:40:36 -0500

    first commit

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# FROM node:16.13.0-alpine AS client_builder
+
+# WORKDIR /app
+
+# COPY ./client .
+
+# RUN npm install --immutable
+
+# RUN npm run build
+
+FROM golang:1.18.1
+
+RUN mkdir /app
+
+WORKDIR $GOPATH/src/github.com/Arturo0911/measurements-realtime
+
+COPY . .
+
+RUN go get -d -v ./...
+
+RUN go install -v ./...
+
+
+# here
+# COPY --from=client_builder /app/dist ./client/dist
+
+EXPOSE 8000
+
+# ENTRYPOINT measurements-realtime --build="go build main.go" --command=./main
+
+CMD ["measurements-realtime"]
---
+++ connection/connection.go
@@ -0,0 +1,41 @@
+package connection
+
+import (
+	"fmt"
+	"log"
+
+	//"os"
+
+	"gorm.io/gorm"
+
+	"gorm.io/driver/mysql"
+)
+
+// const DB_USERNAME = "root"
+// const DB_PASSWORD = "root"
+// USER+":"+PASSWORD+"@tcp(127.0.0.1:3306)/GreenHouseRealTime?parseTime=true&loc=Local"
+
+func NewInstance() *gorm.DB {
+	return GetConnection()
+}
+
+func GetConnection() *gorm.DB {
+
+	//USER := os.Getenv("DB_USER")
+	//PASSWORD := os.Getenv("DB_PASSWORD")
+	//HOST := os.Getenv("DB_HOST")
+	//DBNAME := os.Getenv("DB_NAME")
+
+	//URL := fmt.Sprintf("%s:%s@tcp(godockerDB)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASSWORD, DBNAME)
+	URL := fmt.Sprintf("root_payload:@tcp(127.0.0.1:3306)/GreenHouseRealTime?charset=utf8&parseTime=True&loc=Local")
+
+	db, err := gorm.Open(
+		mysql.Open(URL),
+		&gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error by: %s", err)
+		return nil
+	}
+	fmt.Println("Database connected successfully")
+	return db
+}
